ea934438ebf87c8a8e9239efa6cf9ea8666d4f6f
Raymond Josh Zennda <50371258+raymondzenda14@users.noreply.github.com>
2023-03-01 16:15:12 +0200

    robot app

---
+++ src/robots.js
@@ -0,0 +1,39 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'ray',
+        username: 'rayzee',
+        email: 'rayzee@gmail.com',
+    },
+
+    {
+        id: 2,
+        name: 'steve',
+        username: 'stevezee',
+        email: 'stevezee@gmail.com',
+    },
+
+    {
+        id: 3,
+        name: 'kay',
+        username: 'kayzee',
+        email: 'kaycee@gmail.com',
+    },
+
+    {
+        id: 4,
+        name: 'troy',
+        username: 'troyzee',
+        email: 'troyzee@gmail.com',
+    },
+
+    {
+        id: 5,
+        name: 'kyle',
+        username: 'kylezee',
+        email: 'kylezee@gmail.com',
+    },
+
+
+
+]
